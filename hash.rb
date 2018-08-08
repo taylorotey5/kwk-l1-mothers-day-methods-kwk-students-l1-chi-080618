621c994f3d0bd9dbbdf886c97392ffbb9e55e6cb
Taylor Otey <taylorotey5@gmail.com>
2018-08-08 20:42:09 +0000

    automatically backed up by learn

---
+++ hash.rb
@@ -0,0 +1,12 @@
+fav_celeb = {
+    "Cece" => "Shawn Mendes",
+    "Madi" => "Michael Phelps",
+    "Daren" => "Lebron James",
+    "Halsted" => "Chris Evans"
+    }
+
+fav_celeb.each do [name, celeb]         
+
+puts #{name} admires #{celeb}
+            
+          
